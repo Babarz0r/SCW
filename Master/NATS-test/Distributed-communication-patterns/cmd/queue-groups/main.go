9cfdbb2272a2ab26ef395962f2a471ab8bee1949
Babarz0r <fganderatz@scaleway.com>
2023-01-04 17:01:05 +0100

    fix url

--- Master/NATS-test/Distributed-communication-patterns/cmd/queue-groups/main.go
+++ Master/NATS-test/Distributed-communication-patterns/cmd/queue-groups/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/nats-io/nats.go"
 )
 
 func main() {
-	nc, err := nats.Connect(nats.DefaultURL)
+	servers := []string{"nats://nats.mnq.fr-par.scw.cloud:4222"}
 
+	nc, err := nats.Connect(strings.Join(servers, ","), nats.UserCredentials("admin_crendentials.creds"))
 	if err != nil {
 		log.Fatalln(err)
 	}
